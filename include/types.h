f449b7ccf65f9e7b5fb7c30ec5a8aae82c9d90f8
paulstone <stone.paul.green>
2024-03-12 23:08:24 +0300

    Added error handling mechanism: error codes and function to print a message

--- include/types.h
+++ include/types.h
@@ -3,6 +3,14 @@
 
 /* ================================================================ */
 
+enum linked_list_error {
+    E_OK = 0,           /* Success */
+    E_NULL_V = 1,      /* NULL value */
+    E_NOMEM = 2,       /* Out of memory */
+    E_MISMET = 3,      /* Missing list method */
+    E_MATCH = 4,       /* A node doesn't belong to the list */
+};
+
 /**
  * \brief A pointer to an incomplete data type, allowing user interaction only through a defined interface.
  */
